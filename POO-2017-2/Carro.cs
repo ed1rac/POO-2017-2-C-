3ad250114640533deb0e5412455da8339556d995
ed1rac <edkallenn@gmail.com>
2017-09-09 00:13:42 -0500

    Primeiro commit com as primeiras classes

---
+++ POO-2017-2/Carro.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_2017_2
+{
+    class Carro
+    {
+        private string motor;
+        public int anoDeFabricacao;
+        public string modelo;
+        public int quantMarchas;
+        public bool tracao;
+        public int aroRodas;
+        public string tipoCarro;
+        public string cor;
+
+        public void ligar()
+        {
+            Console.WriteLine("O carro " + this.modelo + " ligou!"); Console.ReadLine();
+        }
+
+        public void acelerar()
+        {
+            Console.WriteLine("O carro " + this.modelo + " está acelerando!"); Console.ReadLine();
+        }
+
+        public void acelerar(int velocidade)
+        {
+            string mensagem = "O carro está acelerando\n";
+            for (int i = 0; i <= velocidade; i += 5)
+            {
+                mensagem += "Velocímetro: " + i + "\t";
+                if (i % 10 == 0)
+                {
+                    mensagem += "\n";
+                }
+            }
+            Console.WriteLine(mensagem); Console.ReadLine();
+        }
+
+        public void frear()
+        {
+            Console.WriteLine("O carro " + modelo + " está freando!"); Console.ReadLine();
+        }
+
+        public void desligar()
+        {
+            Console.WriteLine("O carro " + modelo + " está desligando!"); Console.ReadLine();
+        }
+
+        public void exibeInformacoes()
+        {
+            string mensagem = "";
+            mensagem += "======= INFORMACOES DO CARRO =======" + "\n";
+            mensagem += "Modelo ..........: " + modelo + "\n";
+            mensagem += "Tipo.............: " + tipoCarro + "\n";
+            mensagem += "Ano..............: " + anoDeFabricacao + "\n";
+            mensagem += "Cor..............: " + cor + "\n";
+            mensagem += "Quant. marchas...: " + quantMarchas + "\n";
+            mensagem += "Motor............: " + this.getMotor() + "\n";
+            mensagem += "Rodas de aro.....: " + aroRodas + "\n";
+            if (tracao == true)
+            {
+                mensagem += "Carro traçado!" + "\n";
+            }
+            else
+            {
+                mensagem += "Carro não traçado!" + "\n";
+            }
+            mensagem += "====================================" + "\n";
+
+            Console.WriteLine(mensagem); Console.ReadLine();
+        }
+
+        public void setMotor(String motor)
+        {
+            this.motor = motor;
+        }
+
+        public string getMotor()
+        {
+            return this.motor;
+        }
+    }
+}
---
+++ POO-2017-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_2017_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+
+            //carro.modelo = "Palio";
+            Console.WriteLine("Digite o modelo do carro: ");
+            carro.modelo = Console.ReadLine();
+            carro.tipoCarro = "Hatch";
+            carro.anoDeFabricacao = 2015;
+            carro.aroRodas = 15;
+            carro.cor = "prata";
+            //carro.motor = "1.4";
+            carro.setMotor("1.4");
+            carro.quantMarchas = 4;
+            carro.tracao = false;
+
+            string mensagem = "O modelo é : " + carro.modelo + "\n";
+            mensagem += "O tipo de carro é: " + carro.tipoCarro + "\n";
+            mensagem += "O ano de fabricação é: " + carro.anoDeFabricacao + "\n";
+
+
+            Console.WriteLine(mensagem);
+            carro.ligar();
+            carro.acelerar();
+            carro.acelerar(60); //polimorfismo
+            carro.frear();
+            carro.desligar();
+            carro.exibeInformacoes();
+        }
+    }
+}
